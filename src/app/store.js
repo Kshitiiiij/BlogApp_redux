ce9fad32dfe6bc7dddcb81fbd55d22fa4f9bc003
kshitij <kshitij_987@protonmail.com>
2023-06-22 21:49:11 +0530

    fixed typo

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
-import {confugureStore} from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-export const store = confugureStore({
+export const store = configureStore({
     reducer: {}
 })

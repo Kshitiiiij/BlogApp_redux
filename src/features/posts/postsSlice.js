b24072955e3c2f424a4f0d9eb042f9e9bea6aa4f
kshitij <kshitij_987@protonmail.com>
2023-06-23 19:33:57 +0530

    minor changes

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -3,7 +3,7 @@ import { sub } from "date-fns";
 
 const initialState = [
     { id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString(),
-reations: {
+reactions: {
     thumbsUp: 0,
     wow: 0,
     heart: 0,
@@ -11,7 +11,7 @@ reations: {
     coffee: 0
 }
  },
-    { id: '2', title: 'Second Post', content: 'More text', date:  sub(new Date(), { minutes: 5 }).toISOString(), reations: {
+    { id: '2', title: 'Second Post', content: 'More text', date:  sub(new Date(), { minutes: 5 }).toISOString(), reactions: {
         thumbsUp: 0,
         wow: 0,
         heart: 0,
@@ -37,7 +37,7 @@ const postsSlice = createSlice({
                         content,
                         date: new Date().toISOString(),
                         userId, 
-                        reations: {
+                        reactions: {
                             thumbsUp: 0,
                             wow: 0,
                             heart: 0,
@@ -58,5 +58,5 @@ const postsSlice = createSlice({
     }
 });
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
